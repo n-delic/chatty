ece531d1a2ec80634af465a6e0af9b7547af4b14
Nahid Delic <nahid.delic@fet.ba>
2022-09-09 11:55:46 +0200

    validation v1

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -24,7 +24,12 @@
             <button type="submit" class="button cursor-pointer bg-slightDarkBlue hover:bg-indigo rounded-lg py-2 px-4 text-white shadow-md">
                 Register
             </button>
+
+           
         </form>
+        <p v-for="error of v$.$errors" :key="error.$uid">
+            {{ error.$message }}
+            </p>
     </Card>
 </template>
 
@@ -33,7 +38,7 @@ import Card from '@/components/Card.vue';
 import useAuth from '@/composables/Auth';
 import { reactive } from 'vue';
 import {useVuelidate} from '@vuelidate/core';
-import {required, email} from '@vuelidate/validators';
+import {required, email, sameAs, helpers} from '@vuelidate/validators';
 
 const {register: authRegister} = useAuth();
 
@@ -44,14 +49,29 @@ const state = reactive({
     confirmPassword: ''
 });
 
+const passwordValid = (value: string) => value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)?.length;
+
 const rules = {
-    username: {required},
-    email: {required, email}
+    username: {
+        required: helpers.withMessage('Username is required', required)
+    },
+    email: {
+        required: helpers.withMessage('Email is required',required),
+        email: helpers.withMessage('Email not valid', email)
+    },
+    password: {
+        required: helpers.withMessage('Password is required',required),
+        passwordValid: helpers.withMessage('Password must be min 8 chars, alphanumeric and with special char',passwordValid)  
+    },
+    confirmPassword: {
+        sameAs: helpers.withMessage('Passwords must match',sameAs(state.password))
+    }
 }
 
 const v$ = useVuelidate(rules,state);
 
 const register = async () => {
+    await v$.value.$validate();
     //let statei = await authRegister(state.email,state.password);
     console.log(v$.value);
 }
